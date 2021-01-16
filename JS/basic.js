e02e5cbf040258c97e498f75c3f8b2338ccfc225
witimlfl <939806910@qq.com>
2021-01-16 15:10:21 +0800

    chore: :pencil:  添加防抖debounce函数

--- JS/basic.js
+++ JS/basic.js
@@ -28,5 +28,36 @@ function newThrottle(fn, delay) { // 定时器
             }, delay)
         }
     }
+}
 
+// 防抖debounce: 当持续触发事件时，一定时间段内没有再触发事件，事件处理函数才会执行一次，如果设定的时间到来之前，又一次触发了事件，就重新开始延时。立即执行、非立即执行
+// 立即执行：触发事件后函数会立即执行，然后 n 秒内不触发事件才能继续执行函数的效果
+function debounce(fn, delay) {
+    let timer = null;
+    return function() {
+        const that = this;
+        const args = arguments;
+        if(timer) {
+            clearTimeout(timer);
+        }else {
+            fn.apply(that, args)
+        }
+        timer = setTimeout(() =>{
+            timer = null
+        }, delay)
+    }
+}
+// 非立即执行版:  触发事件后函数不会立即执行，而是在 n 秒后执行，如果在 n 秒内又触发了事件，则会重新计算函数执行时间
+function newDebounce(fn, delay) {
+    let timer = null
+    return function() {
+        const that = this;
+        const args = arguments;
+        if(timer) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(() => {
+            fn.apply(that, args)
+        }, delay)
+    }
 }
